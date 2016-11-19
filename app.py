ce111a449ea263b480e41211b63105b04e16b956
Krish Dholakiya <krishna.dholakiya@gmail.com>
2016-11-19 04:52:49 -0700

    Added port

--- app.py
+++ app.py
@@ -9,21 +9,22 @@ app = Flask(__name__)
 @app.route('/chat', methods=["GET"])
 def index():
 
-    userSpeech = request.args.get('user').encode('utf-8')
+  userSpeech = request.args.get('user').encode('utf-8')
 
-    newSpeech = ""
+  newSpeech = ""
 
-    with open('raw.txt') as f:
-      rawText = f.read()
+  with open('raw.txt') as f:
+    rawText = f.read()
 
-    model = markovify.Text(rawText, state_size=1)
+  model = markovify.Text(rawText, state_size=1)
 
-    for i in range(1):
-      newSpeech += " " + model.make_short_sentence(140)
+  for i in range(1):
+    newSpeech += " " + model.make_short_sentence(140)
 
-    return jsonify({
-      "content": newSpeech
-    })
+  return jsonify({
+    "content": newSpeech
+  })
 
 if __name__ == "__main__":
-    app.run(debug=True)
+  port = int(os.environ.get("PORT", 5000))
+  app.run(host='0.0.0.0', port=port)
