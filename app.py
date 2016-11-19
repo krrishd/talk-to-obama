d80fa070ca7e5f2c33bda04698ccb9e0c576a2d6
Krish Dholakiya <krishna.dholakiya@gmail.com>
2016-11-19 05:09:49 -0700

    turned on debugging

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route('/chat', methods=["GET"])
 def index():
-  
+
   newSpeech = ""
 
   with open('raw.txt') as f:
@@ -26,4 +26,4 @@ def index():
 
 if __name__ == "__main__":
   port = int(os.environ.get("PORT", 5000))
-  app.run(host='0.0.0.0', port=port)
+  app.run(host='0.0.0.0', port=port, debug=True)
